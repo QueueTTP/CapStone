bedd0f26703c3f26b3f759a00ccafc3d848a3d68
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-09-05 09:23:57 -0400

    test kafka

---
+++ Kafka/test/consumer.py
@@ -0,0 +1,16 @@
+from kafka import KafkaConsumer
+import json
+
+# create a consumer object
+consumer = KafkaConsumer('event-simulator',
+                         bootstrap_servers='localhost:9092',
+                            auto_offset_reset='earliest',
+                            enable_auto_commit=True,
+                            group_id='event-group',
+                         value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+
+# consume messages from kafka
+for message in consumer:
+    event = message.value
+    print(f"received event: {event}")
+    
